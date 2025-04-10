3523691faa4d953095cf38675cc0945e91595602
breadjoo <yoohwanjoo@nate.com>
2025-04-10 20:20:19 +0900

    [back] 컬럼명 변경(title)

--- server/src/main/resources/config/mysql/create.sql
+++ server/src/main/resources/config/mysql/create.sql
@@ -3,7 +3,7 @@ use rgt;
 DROP TABLE IF EXISTS `books`;
 CREATE TABLE `books` (
                          `id`	int auto_increment primary key	NOT NULL,
-                         `name`	varchar(250)	NULL,
+                         `title`	varchar(250)	NULL,
                          `author`	varchar(250)	NULL,
                          `description`	text	NULL,
                          `price`	int	NULL,
